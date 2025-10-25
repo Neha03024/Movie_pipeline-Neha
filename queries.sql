794d960963cbb9d30391989ccc7448345d460b8c
Neha suresh <148104145+Neha03024@users.noreply.github.com>
2025-10-25 20:29:49 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,59 +1,37 @@
 1. Which movie has the highest average rating?
-SELECT 
-    m.title,
-    ROUND(AVG(r.rating), 2) AS avg_rating
-FROM 
-    ratings r
-JOIN 
-    movies m ON r.movie_id = m.movie_id
-GROUP BY 
-    m.movie_id
-ORDER BY 
-    avg_rating DESC
+    
+SELECT m.title, ROUND(AVG(r.rating), 2) AS avg_rating
+FROM ratings r
+JOIN movies m ON r.movie_id = m.movie_id
+GROUP BY m.movie_id
+ORDER BY avg_rating DESC
 LIMIT 1;
 
 2.  What are the top 5 movie genres with the highest average rating?
-SELECT 
-    mg.genre,
-    ROUND(AVG(r.rating), 2) AS avg_rating,
-    COUNT(DISTINCT m.movie_id) AS movie_count
-FROM 
-    movie_genres mg
-JOIN 
-    movies m ON mg.movie_id = m.movie_id
-JOIN 
-    ratings r ON r.movie_id = m.movie_id
-GROUP BY 
-    mg.genre
-HAVING 
-    movie_count > 5 
-ORDER BY 
-    avg_rating DESC
+    
+SELECT mg.genre, ROUND(AVG(r.rating), 2) AS avg_rating, COUNT(DISTINCT m.movie_id) AS movie_count
+FROM  movie_genres mg
+JOIN movies m ON mg.movie_id = m.movie_id
+JOIN ratings r ON r.movie_id = m.movie_id
+GROUP BY mg.genre
+HAVING movie_count > 5 
+ORDER BY avg_rating DESC
 LIMIT 5;
 
 3.  Who is the director with the most movies?
-SELECT 
-    md.director,
-    COUNT(DISTINCT md.movie_id) AS total_movies
-FROM 
-    movie_directors md
-GROUP BY 
-    md.director
-ORDER BY 
-    total_movies DESC
+    
+SELECT md.director, COUNT(DISTINCT md.movie_id) AS total_movies
+FROM movie_directors md
+GROUP BY  md.director
+ORDER BY total_movies DESC
 LIMIT 1;
 
 4.  What is the average rating of movies released each year?
-SELECT 
-    m.year,
-    ROUND(AVG(r.rating), 2) AS avg_rating,
-    COUNT(DISTINCT m.movie_id) AS movie_count
-FROM 
-    movies m
-JOIN 
-    ratings r ON m.movie_id = r.movie_id
-WHERE 
-    m.year IS NOT NULL
+    
+SELECT m.year, ROUND(AVG(r.rating), 2) AS avg_rating, COUNT(DISTINCT m.movie_id) AS movie_count
+FROM  movies m
+JOIN ratings r ON m.movie_id = r.movie_id
+WHERE  m.year IS NOT NULL
 GROUP BY 
     m.year
 ORDER BY 
